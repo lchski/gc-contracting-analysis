c7a868c97bb59f4e33db4e63e8ff8eeb1d9d0731
Lucas Cherkewski <lucas@lucascherkewski.com>
2021-11-22 22:15:51 -0500

    make replicable
    (
    replace my count_group helper with the underlying implementation;
    add explanatory comment re data;
    also add some analysis, oops
    )

--- analysis/sole-source.R
+++ analysis/sole-source.R
@@ -7,8 +7,12 @@ sole_sourced_contracts %>%
   mutate(fy = str_remove_all(reporting_period, "-Q[0-9]$")) %>% ## normalize FYs from reporting periods [maybe should use contract date but it's probably fine]
   filter(fy %in% c("2016-2017", "2017-2018", "2018-2019", "2019-2020", "2020-2021", "2021-2022")) %>% ## only years with substantial reporting
   filter(commodity_type %in% c("S", "G")) %>% ## only services or goods, no construction / uncategorized
-  mutate(contract_value_1000s = plyr::round_any(contract_value, 1000, floor)) %>%
-  count_group(fy, commodity_type, contract_value_1000s) %>%
-  top_n(3) %>%
-  arrange(fy, commodity_type, -prop)
+  mutate(contract_value_1000s = plyr::round_any(original_value, 1000, floor)) %>%
+  group_by(fy, commodity_type, contract_value_1000s) %>%
+  summarize(count = n()) %>%
+  mutate(prop = count / sum(count)) %>%
+  top_n(4) %>%
+  arrange(commodity_type, fy, -prop) %>%
+  mutate(prop_magnitude_vs_next = round(prop / lead(prop), 2)) %>%
+  top_n(3)
 
--- load.R
+++ load.R
@@ -1,4 +1,5 @@
 library(tidyverse)
 library(janitor)
 
+# download "Contracts over $10,000" from https://open.canada.ca/data/en/dataset/d8f85d91-7dec-4fd1-8055-483b77225d8b and store in `data/source/...` folder below
 contracts <- read_csv("data/source/open.canada.ca/d8f85d91-7dec-4fd1-8055-483b77225d8b/contracts.csv")
